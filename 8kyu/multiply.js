b8117746a36e3b98b6a56f84366cfef750461d99
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-09-11 12:09:28 -0400

    solition to multiply

---
+++ 8kyu/multiply.js
@@ -0,0 +1,8 @@
+//solution to multiply assignments
+
+function multiply(a, b){
+    console.log(a * b) 
+     return a * b;
+  }
+
+  //link to codewars assignemnt https://www.codewars.com/kata/50654ddff44f800200000004
