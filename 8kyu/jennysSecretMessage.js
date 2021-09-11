4f1bc10c85d53c5f15019e1d30ca50a07f2da580
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-09-11 12:03:03 -0400

    solution for jennys secret

---
+++ 8kyu/jennysSecretMessage.js
@@ -0,0 +1,12 @@
+//solution for jennys secret message
+
+function greet(name){
+    if(name !== "Johnny"){
+      return "Hello, " + name + "!";
+    }
+    if(name == "Johnny"){
+      return "Hello, my love!";
+  }
+    }
+
+    //link to assignment https://www.codewars.com/kata/55225023e1be1ec8bc000390
