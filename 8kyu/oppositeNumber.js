98cf63dfd96bfa6415ffb57db9bc2e5733fe4296
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-09-11 12:07:06 -0400

    solution for opposite number

---
+++ 8kyu/oppositeNumber.js
@@ -0,0 +1,8 @@
+//solution for opposite numbers assignment
+
+function opposite(number) {
+    return number > 0 ? number * -1 : number * -1;
+  }
+  Test.assertEquals(opposite(1), -1,)
+
+  //link to codewars assignment https://www.codewars.com/kata/56dec885c54a926dcd001095
