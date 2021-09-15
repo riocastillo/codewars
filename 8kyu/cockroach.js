048ef4fb51f2cf11cd06d3bcadd3b0c69c45dcb6
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-09-14 22:54:50 -0400

    cockroach commit

---
+++ 8kyu/cockroach.js
@@ -0,0 +1,8 @@
+//solution for codewars assignmnet
+
+function cockroachSpeed(x) {
+    console.log(Math.floor(x * 27.778))
+    return Math.floor(x * 27.778)
+  }
+
+  //https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
