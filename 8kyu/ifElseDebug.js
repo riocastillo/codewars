445875085e42d4b420935e4022b3653aec44118c
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-09-17 18:40:25 -0400

    first commit

---
+++ 8kyu/ifElseDebug.js
@@ -0,0 +1,9 @@
+function checkAlive (health) {
+    if (health <= 0) {
+        return false;
+      } else {
+        return true;
+      }
+     } 
+
+     //solution to codewars: https://www.codewars.com/kata/57089707fe2d01529f00024a
