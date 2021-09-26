94b18d7a2afff12b74fad598312ffda4f5443f59
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-09-26 11:06:45 -0400

    commitcommitcommit

---
+++ 8kyu/convertBoolean.js
@@ -0,0 +1,10 @@
+function boolToWord(bool){
+    if(bool == true){
+      return 'Yes'
+  }
+    else if(bool == false){
+      return 'No'
+    }
+    }
+
+//solution to https://www.codewars.com/kata/53369039d7ab3ac506000467
