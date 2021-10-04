17efeb542eb1c201931d03684d0c2f81b6f9dd83
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-10-04 12:27:54 -0400

    push

---
+++ 8kyu/clockSeries.js
@@ -0,0 +1,9 @@
+function past(h, m, s){
+    hours = h * 3600000
+     minutes = m * 60000
+     seconds = s * 1000
+     time = hours + minutes + seconds
+     return time
+   }
+
+   //solution for codewars https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
--- 8kyu/summationGrasshopper.js
+++
@@ -1,9 +0,0 @@
-//solution to codewars https://www.codewars.com/kata/55d24f55d7dd296eb9000030
-
-var summation = function (num) {
-    let sum = 0
-    for(let i = 0; i <= num; i++) {
-        sum += i
-    }
-    return sum
-}
