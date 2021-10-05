ee3d31196ccd67ac1bd145c7f2d5ef6abf9b6748
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-10-04 23:04:51 -0400

    initial push

---
+++ 8kyu/cardSuite.js
@@ -0,0 +1,15 @@
+function defineSuit(card) {
+    if(card.charAt(card.length-1) === '♣' ){
+     return 'clubs'
+    }
+   else if(card.charAt(card.length-1) === '♠' ){
+     return 'spades'
+    }
+   else if(card.charAt(card.length-1) === '♦' ){
+     return 'diamonds'
+    }
+   else if(card.charAt(card.length-1) === '♥' ){
+      return 'hearts'
+ }}
+
+ //solution for codewars https://www.codewars.com/kata/5a360620f28b82a711000047
