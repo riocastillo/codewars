0888e003267286e90afdd38df6b255e534b578d3
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-10-11 13:18:47 -0400

    sunday codewars

---
+++ 8kyu/trafficLightLogic.js
@@ -0,0 +1,14 @@
+function updateLight(current) {
+    if(current === 'green'){
+      return 'yellow'
+      }
+    
+    if(current === 'yellow'){
+      return 'red' 
+    }
+    if(current === 'red'){
+      return 'green'
+    }
+  }
+
+  //solution for codewars https://www.codewars.com/kata/58649884a1659ed6cb000072
