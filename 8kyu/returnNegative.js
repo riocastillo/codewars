6cb8b1dce2c9e1cabcadaf5c97c4620770bd8778
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-10-13 00:01:15 -0400

    initial push

---
+++ 8kyu/returnNegative.js
@@ -0,0 +1,10 @@
+function makeNegative(num) {
+    if (num <= 0) {
+        return num
+    }
+    else if (num > 0) {
+        return - + num
+    }
+}
+
+    //solution for https://www.codewars.com/kata/55685cd7ad70877c23000102
