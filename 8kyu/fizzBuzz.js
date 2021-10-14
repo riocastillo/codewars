254547ac963ffd7ecb118f4c996b09a7027a668c
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-10-14 08:34:30 -0400

    initial push

---
+++ 8kyu/fizzBuzz.js
@@ -0,0 +1,9 @@
+function preFizz(n) {
+    let newArray = []
+    for(let i=1; i <= n; i++){
+    newArray.push (i)  
+  }
+    return newArray
+  }
+
+  //solution for codewars https://www.codewars.com/kata/569e09850a8e371ab200000b
