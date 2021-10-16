9cd5e7d5a67570b0be554e6e1d6d540ec464dfc2
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-10-16 18:00:49 -0400

    initial push

---
+++ 8kyu/areaOrPerimeter.js
@@ -0,0 +1,10 @@
+function areaOrPerimeter(l , w) {
+    if (l == w){
+      return l * w
+    }
+    else {
+      return (l+w) * 2
+    }
+  };
+
+  //solution for https://www.codewars.com/kata/5ab6538b379d20ad880000ab
