7360e39b5fd7d8feb513ddd2c45d84d81b9ed377
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-11-07 12:07:13 -0500

    initial commit

---
+++ 8kyu/findRemainder.js
@@ -0,0 +1,8 @@
+function remainder(a, b){
+    let largestNum = Math.max(a,b)
+    let smallestNumb = Math.min(a,b)
+    return largestNum % smallestNumb
+     
+   }
+
+   //solution to codewars https://www.codewars.com/kata/524f5125ad9c12894e00003f
