c9c414597cbea26c853ef4535df44fd56e408567
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-11-17 10:13:38 -0500

    Delete twiceAsOld.js

--- 8kyu/twiceAsOld.js
+++
@@ -1,7 +0,0 @@
-function twiceAsOld(dadYearsOld, sonYearsOld) {
-    let twiceOld = dadYearsOld - sonYearsOld * 2
-    return twiceOld < 0 ? twiceOld * (-1) : twiceOld
-  }
-
-  
-  //solution for https://www.codewars.com/kata/5b853229cfde412a470000d0
