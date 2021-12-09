9bb94aa65b89dd2de24987c744507c736871bea3
Rio Acuna Castillo (they/them) <rio.a.castillo@gmail.com>
2021-12-09 14:28:51 -0500

    Add files via upload

---
+++ 7kyu/addProperty.js
@@ -0,0 +1,9 @@
+console.log('questions',typeof questions)
+
+
+questions.forEach((question) => {
+  question.usersAnswer = null
+  console.log(question.usersAnswer,'tester')
+})
+
+//solution for codewars: https://www.codewars.com/kata/54e8c3e89e2ae6f4900005a1
